40432ae6589ccb975cde3aab1dfff72a5c84edcf
Alex Basurto <alexbasurto1@gmail.com>
2023-12-17 12:21:18 +0100

    Regex en login completamente funcional

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,10 +13,35 @@ function Login() {
     const [password, setPassword] = useState('');
     const [error, setError] = useState(null);
 
+    const validateInputs = () => {
+        // Validación de email
+        let errorsAccumulated = '';
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!emailRegex.test(email)) {
+        errorsAccumulated += 'El formato del email no es válido. ';
+        }
+
+        // Validación de contraseña
+        const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8}$/;
+        if (!passwordRegex.test(password)) {
+        errorsAccumulated += 'La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula y un número. ';
+        }
+
+        if (errorsAccumulated !== '') {
+        setError(errorsAccumulated);
+        return false;
+        }
+
+        return true;
+    }
+
     const handleSumbit = (e) => {
         e.preventDefault();
         if (email === '' || password === '') {
             setError('Todos los campos son obligatorios');
+            return;
+        }
+        if (!validateInputs()) {
             return;
         }
         setError(null);
@@ -24,8 +49,7 @@ function Login() {
         .then(response => {
             setSession(response.data);
         }).catch(error => {
-            console.log(error);
-            setError('Error al iniciar sesión');
+            setError('Usuario o contraseña incorrectos');
         });            
     }
 
